999291397c67146e7894f75f7702db2cb4de559f
ronit0000 <ronitkusahu@gmail.com>
2025-11-06 17:34:15 +0530

    Fix: Extend SpringBootServletInitializer for WAR deployment

--- src/main/java/com/klu/ecommerce/EcommerceApplication.java
+++ src/main/java/com/klu/ecommerce/EcommerceApplication.java
@@ -2,9 +2,17 @@ package com.klu.ecommerce;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
+import org.springframework.boot.builder.SpringApplicationBuilder;
 
 @SpringBootApplication
-public class EcommerceApplication {
+public class EcommerceApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(EcommerceApplication.class);
+    }
+    
     public static void main(String[] args) {
         SpringApplication.run(EcommerceApplication.class, args);
     }
